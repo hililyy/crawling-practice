b2bc6ebdd979aa0437920b23755515688009d0fe
hililyy <joun406@gmail.com>
2023-08-04 00:37:51 +0900

    [ADD] 원하는 페이지까지 크롤링

---
+++ Chapter03/5.multiPage.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import pyautogui
+
+keyword = pyautogui.prompt("검색어를 입력하세요 >>")
+lastpage = pyautogui.prompt("마제막 페이지번호를 입력해 주세요")
+pageNum = 1
+for i in range(1, int(lastpage) * 10, 10):
+    print(f"==================  {pageNum}페이지 입니다.  ==================")
+
+    response = requests.get(f"https://m.search.naver.com/search.naver?where=m_news&query={keyword}&start={i}")
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.select(".news_tit") # 결과는 리스트로
+
+    for link in links:
+        title = link.text # 태그 안에 텍스트요소를 가져옴
+        url = link.attrs['href'] # href의 속성값을 가져옴
+        print(title, url)
+    pageNum += 1
